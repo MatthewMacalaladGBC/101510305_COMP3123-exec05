cd356993bfe31c91a0d587d73874080066ff4100
MatthewMacalaladGBC <matthew.macalalad@georgebrown.ca>
2025-10-06 18:03:51 -0400

    partial lab completion

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,27 @@
+// Added proper express setup, as well as fs and path
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const routerUser = express.Router();
+const routerUser = express.Router(); // Adjust all calls from router to routerUser
 
 /*
 - Return all details from user.json file to client as JSON format
 */
-routerUser.get('/profile', (req,res) => {
-  res.send('This is profile router');
+routerUser.get("/profile", (req, res, next) => {
+    fs.readFile(path.join(__dirname, "../user.json"), "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return next(err);
+        }
+        res.json(JSON.parse(data));
+    });
 });
+
+// Error handling middle-ware test
+routerUser.get("/test-error", (req, res, next) => {
+    throw new Error("Error Handling Test")
+});
+
 
 
 
@@ -31,8 +44,8 @@ routerUser.get('/profile', (req,res) => {
         message: "Password is invalid"
     }
 */
-routerUser.post('/login', (req,res) => {
-  res.send('This is login router');
+routerUser.post('/login', (req, res) => {
+    const { username, password } = req.body;
 });
 
 /*
